8f200862a1774f57069cde4a0dfc7c7ca9b42baf
elisvathi <elisvathi@outlook.com>
2020-02-13 11:09:48 +0100

    Fixed shipped package dependencies

--- update-version.js
+++ update-version.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const defPackage = fs.readFileSync(__dirname + "/package.json").toString();
-const version = JSON.parse(defPackage).version;
+const parsed = JSON.parse(defPackage);
+const version = parsed.version;
 console.log(version);
 const prodPackage = JSON.parse(fs.readFileSync(__dirname + "/package-production.json").toString());
 prodPackage.version = version;
+prodPackage.dependencies = parsed.dependencies;
 const prodPackageStr = JSON.stringify(prodPackage, null, 2);
 fs.writeFileSync(__dirname + "/package-production.json", prodPackageStr);

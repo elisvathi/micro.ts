d11e84e2527b527b5c21ba465e30d31b6df54790
elisvathi <elisvathi@outlook.com>
2020-03-18 18:57:02 +0100

    version

--- playground/controllers/UsersController.ts
+++ playground/controllers/UsersController.ts
@@ -1,4 +1,4 @@
-import { JsonController, Get, Param} from '../../src';
+import { JsonController, Get, Param, Body, Post} from '../../src';
 
 @JsonController("users")
 export class UsersController{
@@ -8,4 +8,9 @@ export class UsersController{
     return ["user1", "user2", "user3", param];
   }
 
+  @Post("user")
+  public async testUser(@Body() body: any){
+    console.log("GOT BODY", body);
+    return {body, ok: true, random: Math.random()};
+  }
 }

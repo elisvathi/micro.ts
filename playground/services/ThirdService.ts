f5800bb738afaf82b6adaec6598fa4d1cb59fb16
elisvathi <elisvathi@outlook.com>
2020-12-20 15:26:01 +0100

    Removed circular

--- playground/services/ThirdService.ts
+++ playground/services/ThirdService.ts
@@ -1,8 +1,7 @@
 import { Service } from "../../src";
-import { FirstService } from "./FirstSerivce";
 
 @Service()
 export class ThirdService{
-	constructor(private first: FirstService){
+	constructor(){
 	}
 }

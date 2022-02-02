844cf34f8076f94088efdfd08ec67e65ee8c1df2
Elis Vathi <elisvathi@outlook.com>
2022-02-02 11:14:41 +0100

    Config Updates

--- update-version.js
+++ update-version.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
-const defPackage = fs.readFileSync(__dirname + "/package.json").toString();
+const defPackage = fs.readFileSync(__dirname + '/package.json').toString();
 const parsed = JSON.parse(defPackage);
 const version = parsed.version;
 console.log(version);
-const prodPackage = JSON.parse(fs.readFileSync(__dirname + "/package-production.json").toString());
+const prodPackage = JSON.parse(
+	fs.readFileSync(__dirname + '/package-production.json').toString()
+);
 prodPackage.version = version;
 prodPackage.dependencies = parsed.dependencies;
 const prodPackageStr = JSON.stringify(prodPackage, null, 2);
-fs.writeFileSync(__dirname + "/package-production.json", prodPackageStr);
+fs.writeFileSync(__dirname + '/package-production.json', prodPackageStr);
